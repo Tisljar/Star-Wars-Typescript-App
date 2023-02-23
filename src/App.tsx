e1744fc62774d9e679f29e5a560152b790d49295
Denis Tišljar <denis.tisljar@gmail.com>
2023-02-23 21:24:02 +0100

    Typescript uvod isprobavanje

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from 'react';
+import { ISpecie } from './helpers/interfaces/ISpecie';
+import { getSpecies } from './services/species';
+
 const App = () => {
+    const [species, setSpecies] = useState([]);
+
+    useEffect(() => {
+        const species: Array<ISpecie> = getSpecies('Human', 'Droid', 'Wookie');
+    }, []);
+
     return <></>;
 };
 
--- src/services/species.ts
+++ src/services/species.ts
@@ -1,2 +1,10 @@
-export const getSpecies = (): any => {
-}
+import { ISpecie } from '../helpers/interfaces/ISpecie';
+
+export const getSpecies = (...specieNames: Array<string>): Array<ISpecie> => {
+    const species: Array<ISpecie> = [];
+    try {
+    } catch (error) {
+        console.log(error);
+    }
+    return species;
+};
